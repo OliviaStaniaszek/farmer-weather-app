5bea0b511f96e116a4a84e724c09f7cee7c741f4
chia-program <sanchiasylvester@hotmail.com>
2023-03-20 20:08:08 +0000

    5 day forecast

--- src/components/iphone/index.js
+++ src/components/iphone/index.js
@@ -34,6 +34,10 @@ export default class Iphone extends Component {
 		// Previous url Jane gave
 		//http://api.openweathermap.org/data/2.5/weather?q=London&units=metric&APPID=cf17e23b1d108b29a4d738d2084baf5
 
+
+		//5 day forecast url
+		//http://api.openweathermap.org/data/2.5/forecast?q=London,uk&units=metric&appid=94389e8a8d91186a44a860ea125a4e11
+
 		// API URL with a structure of : ttp://api.wunderground.com/api/key/feature/q/country-code/city.json
 		var url = "http://api.openweathermap.org/data/2.5/weather?q=London,uk&units=metric&appid=94389e8a8d91186a44a860ea125a4e11";
 		$.ajax({
